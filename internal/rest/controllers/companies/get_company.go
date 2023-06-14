eae0a1adc3d6c9d5aeb05f4d42781d843101fe4b
SIIIKOR <siiikor.sikorski@gmail.com>
2023-06-14 19:50:56 +0200

    changed companies/:company_id endpoint

--- internal/rest/controllers/companies/get_company.go
+++ internal/rest/controllers/companies/get_company.go
@@ -12,6 +12,8 @@ import (
 
 	"github.com/msik-404/micro-appoint-gateway/internal/grpc/companies"
 	"github.com/msik-404/micro-appoint-gateway/internal/grpc/companies/companiespb"
+	"github.com/msik-404/micro-appoint-gateway/internal/grpc/employees"
+	"github.com/msik-404/micro-appoint-gateway/internal/grpc/employees/employeespb"
 	"github.com/msik-404/micro-appoint-gateway/internal/rest/middleware"
 )
 
@@ -22,18 +24,18 @@ func GetCompany(c *gin.Context) {
 		return
 	}
 
-	var conn *grpc.ClientConn
-	conn, err := grpc.Dial(companies.ConnString, grpc.WithInsecure())
+	var companiesConn *grpc.ClientConn
+	companiesConn, err := grpc.Dial(companies.ConnString, grpc.WithInsecure())
 	if err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	defer conn.Close()
-	client := companiespb.NewApiClient(conn)
+	defer companiesConn.Close()
+	companiesClient := companiespb.NewApiClient(companiesConn)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	message := companiespb.CompanyRequest{Id: &companyID}
-	reply, err := client.FindOneCompany(ctx, &message)
+	companiesMessage := companiespb.CompanyRequest{Id: &companyID}
+	companiesReply, err := companiesClient.FindOneCompany(ctx, &companiesMessage)
 
 	if err != nil {
 		code := status.Code(err)
@@ -46,5 +48,45 @@ func GetCompany(c *gin.Context) {
 		}
 		return
 	}
-	c.JSON(http.StatusOK, reply)
+
+	var employeesConn *grpc.ClientConn
+	employeesConn, err = grpc.Dial(employees.ConnString, grpc.WithInsecure())
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	defer employeesConn.Close()
+	employeesClient := employeespb.NewApiClient(employeesConn)
+	employeesMessage := employeespb.EmployeesRequest{CompanyId: &companyID}
+	employeesReply, err := employeesClient.FindManyEmployees(ctx, &employeesMessage)
+
+	if err != nil {
+		code := status.Code(err)
+		if code == codes.InvalidArgument {
+			c.AbortWithError(http.StatusBadRequest, err)
+		} else if code == codes.NotFound {
+			c.AbortWithError(http.StatusNotFound, err)
+		} else {
+			c.AbortWithError(http.StatusInternalServerError, err)
+		}
+		return
+	}
+
+	type Response struct {
+		Name            *string                      `json:"name,omitempty"`
+		Type            *string                      `json:"type,omitempty"`
+		Localisation    *string                      `json:"localisation,omitempty"`
+		LongDescription *string                      `json:"long_description,omitempty"`
+		Services        []*companiespb.Service       `json:"services,omitempty"`
+		Employees       []*employeespb.EmployeeShort `json:"employees,omitempty"`
+	}
+	response := Response{
+		Name:            companiesReply.Name,
+		Type:            companiesReply.Type,
+		Localisation:    companiesReply.Localisation,
+		LongDescription: companiesReply.LongDescription,
+		Services:        companiesReply.Services,
+		Employees:       employeesReply.Employees,
+	}
+	c.JSON(http.StatusOK, response)
 }
