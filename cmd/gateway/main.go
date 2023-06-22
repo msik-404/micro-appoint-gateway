09c43edec98c582aee833b4565dbc9b2ac1654cb
SIIIKOR <siiikor.sikorski@gmail.com>
2023-06-22 14:00:52 +0200

    cors fix

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -15,7 +15,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:8080"},
+        AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"content-type", "Authorization", "Access-Control-Allow-Credentials"},
 		AllowCredentials: true,
